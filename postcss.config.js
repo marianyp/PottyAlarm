64099ac496d24ddf65c6f99be037e08e832022dc
Mariany Pinkston <marianypinkston@gmail.com>
2019-05-04 22:35:39 -0400

    Fiddled with postcss stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,23 @@
-const join = require('path').join
-const tailwindJS = join(__dirname, 'tailwind.js')
-
 module.exports = {
-  plugins: [require('tailwindcss')(tailwindJS), require('autoprefixer')]
+  plugins: [
+    require('tailwindcss')('tailwind.js'),
+    // Only add purgecss in production
+    process.env.NODE_ENV === 'production'
+      ? require('@fullhuman/postcss-purgecss')({
+        content: ['./src/**/*.html', './src/**/*.vue'],
+        extractors: [
+          {
+            extractor: class TailwindExtractor {
+              static extract(content) {
+                // eslint-disable-next-line no-useless-escape
+                return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+              }
+            },
+            extensions: ['css', 'html', 'vue']
+          }
+        ]
+      })
+      : '',
+    require('autoprefixer')
+  ]
 }
